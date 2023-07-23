8dd4d2059ad504847b5e37f51de93ac760e08d47
Dzmitry Shiwe <demiyan888@yandex.ru>
2023-07-23 22:43:22 +0300

    feat: add post type

---
+++ src/routes/graphql/types/postType.ts
@@ -0,0 +1,45 @@
+import {
+  GraphQLObjectType,
+  GraphQLNonNull,
+  GraphQLString,
+  GraphQLInputObjectType,
+} from 'graphql';
+import { UUIDType } from './uuid.js';
+import { UserType } from './userType.js';
+import { Context } from './context.js';
+import { Post } from '@prisma/client';
+
+export const PostType = new GraphQLObjectType({
+  name: 'Post',
+  description: 'Post data',
+  fields: () => ({
+    id: { type: UUIDType },
+    title: { type: new GraphQLNonNull(GraphQLString) },
+    content: { type: new GraphQLNonNull(GraphQLString) },
+    authorId: { type: UUIDType },
+    author: {
+      type: new GraphQLNonNull(UserType),
+      resolve: async ({ authorId }: Post, args, { prisma }: Context) => {
+        return await prisma.user.findUnique({ where: { id: authorId } });
+      },
+    },
+  }),
+});
+
+export const CreatePostInputType = new GraphQLInputObjectType({
+  name: 'CreatePostInput',
+  fields: () => ({
+    title: { type: new GraphQLNonNull(GraphQLString) },
+    content: { type: new GraphQLNonNull(GraphQLString) },
+    authorId: { type: new GraphQLNonNull(UUIDType) },
+  }),
+});
+
+export const ChangePostInputType = new GraphQLInputObjectType({
+  name: 'ChangePostInput',
+  fields: () => ({
+    title: { type: GraphQLString },
+    content: { type: GraphQLString },
+    authorId: { type: UUIDType },
+  }),
+});
